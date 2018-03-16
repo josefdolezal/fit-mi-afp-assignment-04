ebe910298a5d44b9f2c44bf0df9997ece244a79f
Josef Dolezal <pepik.ml@gmail.com>
2018-03-17 00:08:01 +0100

    Remove todo comments

--- src/Data/Stack.hs
+++ src/Data/Stack.hs
@@ -9,13 +9,11 @@ empty = Empty
 
 -- Get element from top of stack
 -- If stack is empty error with message "Empty stack" will be raised
--- TODO: implement top
 top :: Stack a -> a
 top (NonEmpty a _) = a
 top _ = error "Empty stack"
 
 -- Get element from top of stack (if there is some, otherwise return Nothing)
--- TODO: implement safe top
 topSafe :: Stack a -> Maybe a
 topSafe (NonEmpty a _) = Just a
 topSafe _ = Nothing
@@ -23,31 +21,26 @@ topSafe _ = Nothing
 
 -- Pop element from top of stack
 -- If stack is empty error with message "Empty stack" will be raised
--- TODO: implement pop
 pop :: Stack a -> Stack a
 pop (NonEmpty _ s) = s
 pop _ = error "Empty stack"
 
 -- Pop element from top of stack (if there is some, otherwise return Nothing)
--- TODO: implement safe pop
 popSafe :: Stack a -> Maybe (Stack a)
 popSafe (NonEmpty _ s) = Just s
 popSafe _ = Nothing
 
 -- Push element to top of stack
--- TODO: implement push
 push :: a -> Stack a -> Stack a
 push a s = NonEmpty a s
 
 -- Get number of elements in stack
--- TODO: implement size
 size :: Num n => Stack a -> n
 size (NonEmpty _ s) = 1 + size s
 size _ = 0
 
 -- Check if stack is empty
 -- Note: is more effective than checking if size is zero
--- TODO: implement null (not by using size!)
 null :: Stack a -> Bool
 null Empty = True
 null _ = False
