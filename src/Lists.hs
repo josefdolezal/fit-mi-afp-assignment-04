314c49086a0b1a4aab2ca64f9a1ea44bed6ee976
Josef Dolezal <pepik.ml@gmail.com>
2018-03-18 18:02:02 +0100

    Add pythogorean triples generator

--- src/Lists.hs
+++ src/Lists.hs
@@ -5,7 +5,8 @@ module Lists where
 -- https://en.wikipedia.org/wiki/Pythagorean_triple
 -- List is ordered by x,y,z in ascending order, contains also primitive triples
 pythagoreanTriples :: [(Integer, Integer, Integer)]
-pythagoreanTriples = undefined
+pythagoreanTriples = [(a, b, c) | c <- [1..], b <- [1..c-1], a <- [1..b-1], valid a b c]
+    where valid a b c = a^2 + b^2 == c^2
 
 -- TODO: create identity matrix (1 on main diagonal, 0 otherwise)
 -- TODO: use list comprehension!
